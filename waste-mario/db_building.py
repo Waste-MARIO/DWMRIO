bb7b0e76a9dcac3e8e9ea7537efba27a3d4224fd
mattiabv <mattia.barberis@mail.polimi.it>
2023-10-05 12:59:52 +0200

    Primi passi Modello Simone

---
+++ waste-mario/db_building.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 11:53:18 2023
+
+@author: loren
+"""
+
+import mario
+import pandas as pd
+
+user = 'LR'
+year = 2011
+paths = pd.read_excel("Paths.xlsx", index_col=[0]).loc[:,"LR"]
+
+#%% Parse exiobase IOT ixi of given year
+world = mario.parse_exiobase(
+    table='IOT',
+    unit='Monetary',
+    path=paths['IOT folder']+f"\\IOT_{year}_ixi.zip"
+    )
+
+#%% creating template for new sectors
+new_sectors = [
+    "Electric Vehicles",
+    "e-bikes",
+    "Wind turbine",
+    "Permanent Magnet",
+    "Cobalt",
+    "Lithium",
+    "Neodymium",
+    "Dysprosium",
+    ]
+
+world.get_add_sectors_excel(
+    new_sectors=new_sectors,
+    regions=world.get_index('Region'),
+    path=paths['Add sectors non waste'],
+    )
+
+#%% adding new empty sectors
+world.add_sectors(
+    new_sectors=new_sectors,
+    regions=world.get_index('Region'),
+    io=paths['Add sectors non waste'],
+    item='Sector',
+    )
+
+#%% aggregating regions
+# world.get_aggregation_excel(...)
+
+#%% export 
+world.to_txt(
+    path=paths['Database']+"\\DB1",
+    # coefficients = True
+    )
+
+
+
